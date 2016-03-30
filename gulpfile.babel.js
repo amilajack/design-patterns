889f0905b47388da47aa116300ad0919ae8f1644
Amila Welihinda <amilajack@gmail.com>
2016-03-29 19:17:13 -0700

    Initial commit, added table of contents

---
+++ gulpfile.babel.js
@@ -0,0 +1,28 @@
+import gulp from 'gulp'
+import del from 'del'
+import babel from 'gulp-babel'
+
+const scripts = [
+  'LeetCode/*',
+  'Math/*',
+  'Reverse/*',
+  'DataStructures/*'
+]
+const distPath = './dist'
+
+// Remove all compiled scripts
+gulp.task('clean', cb => del(['./dist'], { dot: true }))
+
+// Watch scripts
+gulp.task('watch', () => {
+  gulp.watch(scripts, ['default'])
+})
+
+// Default task
+gulp.task('default', ['clean'], () => {
+
+  gulp
+    .src(scripts)
+    .pipe(babel())
+    .pipe(gulp.dest(distPath))
+})
---
+++ readme.md
@@ -0,0 +1,20 @@
+# Contents (Work in Progress)
+
+# Programming Styles
+## Functional Programming
+## Object Oriented Programming
+## Reactive Programming
+## Functional Reactive Programming FRP
+
+# Architectural Patterns
+## Application level design patterns
+* Facade
+* Inversion of Control (IOC)
+* Iterator
+* Observer
+
+# Javascript Design Patterns
+### Patterns that are specific to javascript
+* Currying
+* Factory
+* Decorater
